b378703a225f4ee27d4ad1809a733229147033e6
Adrián Bán <pikapovwva@gmail.com>
2021-04-26 21:25:15 +0200

    bugfix

--- streamlit.py
+++ streamlit.py
@@ -49,24 +49,11 @@ def main():
         st.write("Please select a page on the left.")
         st.write(df)
     elif size == "Exploration":
-        st.title("Data Exploration")
-        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, 
-                              index=3)
-        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, 
-                              index=4)
-        visualize_data(df, x_axis, y_axis)
+        pass
 
 
 
-def visualize_data(df, x_axis, y_axis):
-    graph = alt.Chart(df).mark_circle(size=60).encode(
-        x=x_axis,
-        y=y_axis,
-        color='Origin',
-        tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
-    ).interactive()
 
-    st.write(graph)
 
 if __name__ == "__main__":
     main()
